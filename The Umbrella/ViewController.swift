6a1957fc1dc90c84a95cca70b746077b422dffcd
Mauro Juliano <43898163+MauroJuliano@users.noreply.github.com>
2020-08-05 10:31:20 -0700

    Started Umbrella App
    The umbrella it's a weather app

---
+++ The Umbrella/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  The Umbrella
+//
+//  Created by Lestad on 2020-08-04.
+//  Copyright © 2020 Lestad. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.view.backgroundColor = UIColor(patternImage: UIImage(named: "iphone.png")!)
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
